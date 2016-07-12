74a314f5ed0e5a19f97bc664c220c7e1093a2f49
Carsten B <c.behnert@gmx.de>
2016-07-12 18:58:10 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'static_pages/rules'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+
+  def setup
+  	@base_title = "| Flatshare App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home #{@base_title}"
+  end
+
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home #{@base_title}"
+  end
+
+  test "should get rules" do
+    get static_pages_rules_url
+    assert_response :success
+    assert_select "title", "Rules #{@base_title}"
+  end
 end
