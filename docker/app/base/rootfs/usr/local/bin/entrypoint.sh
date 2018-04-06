2e5fd9e5edfb041c6e091b23c1ed6bfcf191203b
Carsten Behnert <c.behnert@gmx.de>
2018-04-06 23:40:07 +0200

    Update docker setup

--- docker/app/base/rootfs/usr/local/bin/entrypoint.sh
+++ docker/app/base/rootfs/usr/local/bin/entrypoint.sh
@@ -2,23 +2,21 @@
 
 set -e
 
-# TODO: check if SQLite is used for production
-# TODO: if DBMS is integrated, check rake tasks if db:setup can be executed conditionally
+# Check if SQLite is used for production
+# and if DBMS is integrated, check rake tasks if db:setup can be executed conditionally
 if [ ! -f /data/production.sqlite3 ]; then
 	echo "Database not existing, executing setup..."
 	bundle exec rake db:setup RAILS_ENV=production
-
-	chown -R www-data:www-data /data/
-	# passenger runs as nobody:nobody
-	chmod -R 777 /data/
 else
 	echo "Database existing, running migrations..."
 	bundle exec rake db:migrate RAILS_ENV=production
-
-	chown -R www-data:www-data /data/
-	# passenger runs as nobody:nobody
-	chmod -R 777 /data/
 fi
+
+chown -R www-data:www-data /data/
+# passenger runs as nobody:nobody
+chmod -R 777 /data/
+echo "Database established"
+
 
 # for DBMS (pseudo code)
 #if [ bundle exec rake db:needs-setup RAILS_ENV=production 1>/dev/null 2>&1 ]; then
