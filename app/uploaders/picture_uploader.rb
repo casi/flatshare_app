3667089eb2b876a14ef5fc4ffd42e6049c3f0549
Carsten Behnert <c.behnert@gmx.de>
2018-04-09 22:48:10 +0200

    Add default profile pic
    + improvement of the picture uploader
    + removal of picture check

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
+
 class PictureUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [100, 100]
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
+  version :default do
+    process resize_to_limit: [110, 110]
   end
 
+  version :thumb, from_version: :default do
+    process resize_to_limit: [75, 75]
+  end
+
+  def extension_white_list
+    %w[jpg jpeg gif png]
+  end
 end
