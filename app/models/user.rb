dcabf2fbb9f456ed36944e4e3fbcea70f7606e48
Carsten Behnert <c.behnert@gmx.de>
2019-05-03 16:29:20 +0200

    Lint model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,26 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :infos
   has_many :comments
 
-  before_save { email.downcase! }
+  before_save :downcase_email
 
-  default_scope -> { where("id > 0").order(created_at: :desc) }
+  default_scope -> { where('id > 0').order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
 
-  validates :name, presence: true, length: { maximum: 50}
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false}
+                    uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   validate :picture_size
 
   # Returns the hash digest of the given string. (for fixtures)
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
@@ -26,11 +28,14 @@ class User < ApplicationRecord
 
   private
 
+  def downcase_email
+    self.email = email.downcase
+  end
+
   # validates the size of an uploaded picture
   def picture_size
     if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5 MB")
+      errors.add(:picture, 'should be less than 5 MB')
     end
   end
-
 end
