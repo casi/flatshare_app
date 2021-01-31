cbc48f5f61e0129f7923dc17c92e5a99f1525652
Carsten Behnert <c.behnert@gmx.de>
2021-01-31 19:25:21 +0100

    Minor test change / user access

--- test/controllers/qandas_controller_test.rb
+++ test/controllers/qandas_controller_test.rb
@@ -4,7 +4,8 @@ class QandasControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @qanda = qandas(:one)
-    @user = users(:carsten)
+    @user  = users(:carsten)
+    @user2 = users(:tina)
   end
 
   test "should get index" do
@@ -16,11 +17,13 @@ class QandasControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    log_in_as @user
     get new_qanda_url
     assert_response :success
   end
 
   test "should create qanda" do
+    log_in_as @user
     assert_difference('Qanda.count') do
 		post qandas_url, params: { qanda: { question: @qanda.question, 
                                         answer: @qanda.answer } }
@@ -30,11 +33,13 @@ class QandasControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    log_in_as @user2
     get edit_qanda_url(@qanda)
     assert_response :success
   end
 
   test "should update qanda" do
+    log_in_as @user2
     patch qanda_url(@qanda), params: { qanda: { question: @qanda.question, 
                                             	  answer: @qanda.answer } }
     assert_redirected_to qandas_url
