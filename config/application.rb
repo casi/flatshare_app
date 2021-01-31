7fb52c0e7ec7c8c57ef17614b0122a09b5c1ec17
Carsten Behnert <c.behnert@gmx.de>
2021-01-31 19:56:43 +0100

    Remove automated versioning
    * I don't see any point in it anymore. I will do it manually in the future, along with tagging.

--- config/application.rb
+++ config/application.rb
@@ -14,14 +14,6 @@ module FlatshareApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Version. Only attempt update on local machine
-    if Rails.env.development?
-      # Update version file from latest git tag
-      File.open('config/version', 'w') do |file|
-        file.write `git describe --tags --always`
-      end
-    end
-
     config.version = File.read('config/version')
     config.time_zone = 'Berlin'
     config.active_record.default_timezone = :local
